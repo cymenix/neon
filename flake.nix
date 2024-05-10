e43747881cb80da3eef3358502374ce5c114ecac
Clemens Horn <clemens.horn@mni.thm.de>
2024-05-10 22:33:47 +0200

    chore: build with flake using makefile

--- flake.nix
+++ flake.nix
@@ -34,6 +34,18 @@
           extensions = ["rust-src" "clippy" "llvm-tools"];
         };
         buildInputs = with pkgs; [
+          readline
+          zlib
+          icu
+          perl
+          openssl
+          libseccomp
+          curl
+          protobuf
+          pkg-config
+          rustPlatform.bindgenHook
+        ];
+        nativeBuildInputs = with pkgs; [
           coreutils
           bash
           openssl
@@ -41,23 +53,103 @@
           flex
           bison
           icu
-          readline
-          zlib
           libseccomp
           curl
+          perl
           protobuf
           rustPlatform.bindgenHook
         ];
-        nativeBuildInputs = with pkgs; [
-          rustToolchain
-          rust-analyzer
-        ];
-      in {
-        devShell = pkgs.mkShell {
-          inherit buildInputs nativeBuildInputs;
-          RUST_BACKTRACE = 1;
-          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-        };
-      }
+      in
+        with pkgs; {
+          packages = {
+            default = rustPlatform.buildRustPackage {
+              inherit buildInputs nativeBuildInputs;
+              name = "neondb";
+
+              src = self;
+
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                outputHashes = {
+                  "heapless-0.8.0" = "sha256-phCls7RQZV0uYhDEp0GIphTBw0cXcurpqvzQCAionhs=";
+                  "postgres-0.19.4" = "sha256-avMTCF5BRgSun1etqj+edVGMifyv3FxF0pS0WQ+CF5E=";
+                  "parquet-49.0.0" = "sha256-E5KuNB9O+yOvnvrB4WjDNGSbsWLdFvcSNz9xBfUL3ac=";
+                  "svg_fmt-0.4.2" = "sha256-+UyAowTBKc+3NJWJOH74cxV4l04UOH67KGac/NrJL1M=";
+                  "tokio-epoll-uring-0.1.0" = "sha256-R33KZtNMRy34C0Ea9jgd1Jp9ckU+gHW8ufwC9sqOgiM=";
+                };
+              };
+
+              postPatch = ''
+                mkdir -p pg_install pg_install/build
+                substituteInPlace ./Makefile \
+                  --replace-fail "&& git rev-parse HEAD" ""
+
+                substituteInPlace ./scripts/ninstall.sh \
+                  --replace-fail /bin/bash ${pkgs.bash}/bin/bash
+
+                substituteInPlace ./vendor/postgres-v14/configure \
+                  --replace-fail /bin/pwd ${pkgs.coreutils}/bin/pwd
+
+                substituteInPlace ./vendor/postgres-v15/configure \
+                  --replace-fail /bin/pwd ${pkgs.coreutils}/bin/pwd
+
+                substituteInPlace ./vendor/postgres-v16/configure \
+                  --replace-fail /bin/pwd ${pkgs.coreutils}/bin/pwd
+
+                substituteInPlace ./vendor/postgres-v14/configure.ac \
+                  --replace-fail /bin/pwd ${pkgs.coreutils}/bin/pwd
+
+                substituteInPlace ./vendor/postgres-v15/configure.ac \
+                  --replace-fail /bin/pwd ${pkgs.coreutils}/bin/pwd
+
+                substituteInPlace ./vendor/postgres-v16/configure.ac \
+                  --replace-fail /bin/pwd ${pkgs.coreutils}/bin/pwd
+              '';
+
+              BUILD_TYPE = "release";
+
+              CARGO_BUILD_FLAGS = "--features=testing";
+
+              buildPhase = ''
+                make -j`nproc` -s postgres-v16 neon-pg-ext-v16
+                export PQ_LIB_DIR="$(pwd)/pg_install/v16/lib";
+                export LD_LIBRARY_PATH="$PQ_LIB_DIR:$LD_LIBRARY_PATH"
+                make -j`nproc` -s neon
+              '';
+
+              installPhase = ''
+                mkdir -p $out/bin $out/pg_install
+                cp -r pg_install/* $out/pg_install/
+                cp target/release/neon_local $out/bin
+                cp target/release/safekeeper $out/bin
+                cp target/release/pageserver $out/bin
+                cp target/release/storage_broker $out/bin
+                cp target/release/storage_controller $out/bin
+                cp target/release/proxy $out/bin
+                cp target/release/pagectl $out/bin
+                cp target/release/compute_ctl $out/bin
+                cp target/release/storcon_cli $out/bin
+                cp target/release/pagebench $out/bin
+                cp target/release/trace $out/bin
+                cp target/release/vm-monitor $out/bin
+                cp target/release/wal_craft $out/bin
+              '';
+
+              doCheck = false;
+              dontFixup = true;
+            };
+          };
+          devShell = mkShell {
+            inherit buildInputs;
+            nativeBuildInputs =
+              nativeBuildInputs
+              ++ [
+                rustToolchain
+                rust-analyzer
+              ];
+            RUST_BACKTRACE = 1;
+            RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+        }
     );
 }
